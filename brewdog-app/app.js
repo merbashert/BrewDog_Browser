88ad53db1ea28600fbc8226f380ebe703f7c4db7
Meredith Bashert <merbashert@gmail.com>
2019-10-18 12:47:25 -0400

    abv AJAX functioning

--- brewdog-app/app.js
+++ brewdog-app/app.js
@@ -1,16 +1,5 @@
 $(() => {
 
-
-    // const userInput = $('#name').val();
-    // const minABV = $('#minABV').val();
-    // const maxABV = $('#maxABV').val();
-    // const minIBU = $('#minIBU').val();
-    // const maxIBU = $('#maxIBU').val();
-    // const food = $('#food').val();
-    //
-
-
-
     $('#nameButton').on('click', (event) => {
         event.preventDefault();
 
@@ -26,6 +15,7 @@ $(() => {
                     const $name = $('<div>').appendTo($beerInfo).addClass('name').text(data[i].name)
                     const $desc = $('<div>').appendTo($beerInfo).addClass('description').text(data[i].description)
                     const $abv = $('<div>').appendTo($beerInfo).addClass('abv').text("ABV: " + data[i].abv)
+                    const $ibu = $('<div>').appendTo($beerInfo).addClass('ibu').text("IBU: " + data[i].ibu)
                     const $food = $('<div>').appendTo($beerInfo).addClass('food').text("Food pairings: " + data[i].food_pairing)
                 }
             }
@@ -36,12 +26,39 @@ $(() => {
         );
     }); //end of name button
 
+    $('#abvButton').on('click', (event) => {
+        event.preventDefault();
+
+        const minABV = $('#minABV').val();
+        const maxABV = $('#maxABV').val();
+
+        const abvAPI = $.ajax({
+            url: 'https://api.punkapi.com/v2/beers?abv_gt=' + minABV + '&abv_lt=' + maxABV
+        }).then(
+            (data) => {
+                for (let i = 0; i < 5; i++) {
+                    const $beerInfo = $('<div>').addClass("beer-info")
+                    $('#search-result').append($beerInfo)
+                    const $name = $('<div>').appendTo($beerInfo).addClass('name').text(data[i].name)
+                    const $desc = $('<div>').appendTo($beerInfo).addClass('description').text(data[i].description)
+                    const $abv = $('<div>').appendTo($beerInfo).addClass('abv').text("ABV: " + data[i].abv)
+                    const $ibu = $('<div>').appendTo($beerInfo).addClass('ibu').text("IBU: " + data[i].ibu)
+                    const $food = $('<div>').appendTo($beerInfo).addClass('food').text("Food pairings: " + data[i].food_pairing)
+                }
+            }
+            // ,
+            // () => {
+            //     alert("What's the name?");
+            // }
+        );
+    }); //end of abv button
+
     $('#foodButton').on('click', (event) => {
         event.preventDefault();
 
         const food = $('#food').val();
 
-        const promise = $.ajax({
+        const foodAPI = $.ajax({
             url: 'https://api.punkapi.com/v2/beers?food=' + food
         }).then(
             (data) => {
@@ -51,6 +68,7 @@ $(() => {
                     const $name = $('<div>').appendTo($beerInfo).addClass('name').text(data[i].name)
                     const $desc = $('<div>').appendTo($beerInfo).addClass('description').text(data[i].description)
                     const $abv = $('<div>').appendTo($beerInfo).addClass('abv').text("ABV: " + data[i].abv)
+                    const $ibu = $('<div>').appendTo($beerInfo).addClass('ibu').text("IBU: " + data[i].ibu)
                     const $food = $('<div>').appendTo($beerInfo).addClass('food').text("Food pairings: " + data[i].food_pairing)
                 }
             }
