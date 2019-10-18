ccfdae955c9d293cd7e6c26444c37406200cc79e
Meredith Bashert <merbashert@gmail.com>
2019-10-18 12:37:12 -0400

    food AJAX functioning

--- brewdog-app/app.js
+++ brewdog-app/app.js
@@ -11,12 +11,12 @@ $(() => {
 
 
 
-    $('#name').on('click', (event) => {
+    $('#nameButton').on('click', (event) => {
         event.preventDefault();
 
-        const beerName = $('input[type="text"]').val();
+        const beerName = $('#name').val();
 
-        const promise = $.ajax({
+        const beerAPI = $.ajax({
             url: 'https://api.punkapi.com/v2/beers?beer_name=' + beerName
         }).then(
             (data) => {
@@ -26,12 +26,39 @@ $(() => {
                     const $name = $('<div>').appendTo($beerInfo).addClass('name').text(data[i].name)
                     const $desc = $('<div>').appendTo($beerInfo).addClass('description').text(data[i].description)
                     const $abv = $('<div>').appendTo($beerInfo).addClass('abv').text("ABV: " + data[i].abv)
+                    const $food = $('<div>').appendTo($beerInfo).addClass('food').text("Food pairings: " + data[i].food_pairing)
                 }
-            },
-            () => {
-                alert("What's the name?");
             }
+            // ,
+            // () => {
+            //     alert("What's the name?");
+            // }
         );
     }); //end of name button
+
+    $('#foodButton').on('click', (event) => {
+        event.preventDefault();
+
+        const food = $('#food').val();
+
+        const promise = $.ajax({
+            url: 'https://api.punkapi.com/v2/beers?food=' + food
+        }).then(
+            (data) => {
+                for (let i = 0; i < 5; i++) {
+                    const $beerInfo = $('<div>').addClass("beer-info")
+                    $('#search-result').append($beerInfo)
+                    const $name = $('<div>').appendTo($beerInfo).addClass('name').text(data[i].name)
+                    const $desc = $('<div>').appendTo($beerInfo).addClass('description').text(data[i].description)
+                    const $abv = $('<div>').appendTo($beerInfo).addClass('abv').text("ABV: " + data[i].abv)
+                    const $food = $('<div>').appendTo($beerInfo).addClass('food').text("Food pairings: " + data[i].food_pairing)
+                }
+            }
+            // ,
+            // () => {
+            //     alert("What's the name?");
+            // }
+        );
+    }); //end of food button
 
 }) //end of onload
